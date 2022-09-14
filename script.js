8927202734974ad87aa260230fc63a9b0fda9b83
rodolfo-gm <gutierrezrodolfo80@gmail.com>
2022-09-14 17:50:39 -0400

    third commit // finsihed homepage

---
+++ script.js
@@ -0,0 +1,18 @@
+const bar = document.querySelector("#bar");
+const nav = document.querySelector("#navbar"); 
+const close = document.querySelector("#close");
+
+if(bar){
+    bar.addEventListener('click', ()=>{
+        nav.classList.add('active');
+        bar.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener('click', ()=>{
+        nav.classList.remove('active');
+        bar.classList.remove('active');
+    })
+}
+
